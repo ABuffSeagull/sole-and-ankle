c1bd3130c35115c96c529018c217f9e9f5f254b1
Reece Van Atta <reece@thrasker.com>
2024-08-23 18:25:21 -0400

    Pull out components

--- src/Main.elm
+++ src/Main.elm
@@ -40,26 +40,34 @@ update msg model =
 view model =
     { title = "Sole and Ankle"
     , body =
-        [ div [ class "bg-gray-900 px-8 py-2 flex text-gray-300 gap-6" ]
-            [ div [ class "mr-auto text-white" ] [ text "Free shipping on domestic orders over $75!" ]
-            , label [ class "border-b border-gray-300 relative" ]
-                [ icon Search [ SAttr.class "size-4 absolute my-auto inset-y-0 left-0" ]
-                , input [ Attr.placeholder "Search...", class "pl-6 bg-transparent placeholder:text-gray-500" ] []
-                ]
-            , a [] [ text "Help" ]
-            , icon ShoppingBag [ SAttr.class "size-6" ]
-            ]
-        , nav [ class "flex justify-center relative items-end gap-12 font-medium py-5 border-b border-gray-300 text-lg" ]
-            [ a [ Attr.href "/", class "font-bold text-2xl absolute left-8" ] [ text "Sole&Ankle" ]
-            , a [ Attr.href "#", class "uppercase text-secondary" ] [ text "Sale" ]
-            , a [ Attr.href "#", class "uppercase" ] [ text "New Releases" ]
-            , a [ Attr.href "#", class "uppercase" ] [ text "Men" ]
-            , a [ Attr.href "#", class "uppercase" ] [ text "Women" ]
-            , a [ Attr.href "#", class "uppercase" ] [ text "Kids" ]
-            , a [ Attr.href "#", class "uppercase" ] [ text "Collections" ]
-            ]
+        [ superHeader
+        , header
         ]
     }
+
+
+superHeader =
+    div [ class "bg-gray-900 px-8 py-2 flex text-gray-300 gap-6" ]
+        [ div [ class "mr-auto text-white" ] [ text "Free shipping on domestic orders over $75!" ]
+        , label [ class "border-b border-gray-300 relative" ]
+            [ icon Search [ SAttr.class "size-4 absolute my-auto inset-y-0 left-0" ]
+            , input [ Attr.placeholder "Search...", class "pl-6 bg-transparent placeholder:text-gray-500" ] []
+            ]
+        , a [] [ text "Help" ]
+        , icon ShoppingBag [ SAttr.class "size-6" ]
+        ]
+
+
+header =
+    nav [ class "flex justify-center relative items-end gap-12 font-medium py-5 border-b border-gray-300 text-lg" ]
+        [ a [ Attr.href "/", class "font-bold text-2xl absolute left-8" ] [ text "Sole&Ankle" ]
+        , a [ Attr.href "#", class "uppercase text-secondary" ] [ text "Sale" ]
+        , a [ Attr.href "#", class "uppercase" ] [ text "New Releases" ]
+        , a [ Attr.href "#", class "uppercase" ] [ text "Men" ]
+        , a [ Attr.href "#", class "uppercase" ] [ text "Women" ]
+        , a [ Attr.href "#", class "uppercase" ] [ text "Kids" ]
+        , a [ Attr.href "#", class "uppercase" ] [ text "Collections" ]
+        ]
 
 
 type Icon
