40ef0b6b67a9e59336433ab58712cd42f6ee04c3
Reece Van Atta <reece@thrasker.com>
2024-08-23 18:09:38 -0400

    Superheader

--- src/Main.elm
+++ src/Main.elm
@@ -2,7 +2,9 @@ module Main exposing (main)
 
 import Browser
 import Html exposing (..)
-import Html.Attributes exposing (class, classList)
+import Html.Attributes as Attr exposing (class, classList)
+import Svg
+import Svg.Attributes as SAttr
 
 
 main : Program () Model Msg
@@ -37,5 +39,45 @@ update msg model =
 
 view model =
     { title = "Sole and Ankle"
-    , body = []
+    , body =
+        [ div [ class "bg-gray-900 px-8 py-2 flex text-gray-300 gap-6" ]
+            [ div [ class "mr-auto text-white" ] [ text "Free shipping on domestic orders over $75!" ]
+            , label [ class "border-b border-gray-300 relative" ]
+                [ icon Search [ SAttr.class "size-4 absolute my-auto inset-y-0 left-0" ]
+                , input [ Attr.placeholder "Search...", class "pl-6 bg-transparent placeholder:text-gray-500" ] []
+                ]
+            , a [] [ text "Help" ]
+            , icon ShoppingBag [ SAttr.class "size-6" ]
+            ]
+        ]
     }
+
+
+type Icon
+    = Search
+    | ShoppingBag
+    | ChevronDown
+    | Menu
+    | X
+
+
+icon id attr =
+    let
+        strId =
+            case id of
+                Search ->
+                    "search"
+
+                ShoppingBag ->
+                    "shopping-bag"
+
+                ChevronDown ->
+                    "chevron-down"
+
+                Menu ->
+                    "menu"
+
+                X ->
+                    "x"
+    in
+    Svg.svg attr [ Svg.use [ SAttr.xlinkHref ("/icons.svg#" ++ strId) ] [] ]
